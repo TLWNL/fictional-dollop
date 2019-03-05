36d4bc22c4c07860afb54b03407c536f08716279
TLWNL <dylanrijnsburger@gmail.com>
2019-03-05 14:52:51 +0100

    Magnie

--- CircularLineBuffer.cpp
+++ CircularLineBuffer.cpp
@@ -23,17 +23,7 @@ int CircularLineBuffer::nextFreeIndex() {
     if(isFull())
         return -1;
 
-    return start + count;           // I think
-
-//    for(int i = start; i<bufferSize; i++){
-//       if(buffer[i] == 0)
-//           return i;
-//    }
-//    for(int j = 0; j<start; j++){
-//        if(buffer[j] == 0)
-//            return j;
-//    }
-//    return -2;
+    return start + count;
 }
 
 int CircularLineBuffer::findNewline() {
@@ -55,12 +45,15 @@ bool CircularLineBuffer::hasLine() {
 bool CircularLineBuffer::_writeChars(const char *chars, size_t nchars) {
     bool loop = false;
     int chars_written = 0;
-//
-//    if(nchars > freeSpace())
-//        return false;
+    int start_point = nextFreeIndex();
 
-    for(int i = nextFreeIndex(); i< start + nchars; i++){
-        if(i == bufferSize) {
+    if(nchars > freeSpace()) {
+        std::cout << "Not enough space in the buffer!\n";
+        return false;
+    }
+
+    for(int i = start_point; i< start_point + nchars; i++){
+        if(i >= bufferSize) {
             loop = true;
             break;
         }
@@ -69,63 +62,48 @@ bool CircularLineBuffer::_writeChars(const char *chars, size_t nchars) {
         chars_written++;
     }
     if(loop){
-//        std::cout << "Looping \/n";
-        for(int j = 0; j < nchars - chars_written +1; j++){
-//            if(j == start)
-//                return false;
-            buffer[j] = chars[chars_written];
+        std::cout << "Looping \n";
+        int magnie;
+        int chars_left = nchars - chars_written;
+
+        while(chars_left != 0){
+            magnie = (start+count) - bufferSize;
+            buffer[magnie] = chars[chars_written];
             chars_written++;
+            chars_left--;
             count++;
         }
+
     }
     return true;
 }
 
 std::string CircularLineBuffer::_readLine() {
     std::string return_string;
-//    int newline_index = findNewline();
-    bool loop = false;
 
     if(findNewline() == -1)
         return return_string;               // Return empty string if there is no full line
 
-    for(int i = start; i < bufferSize; i++){            // change i < bufferSize to i < findNewLine()
+    for(int i = start; i < findNewline() +1; i++){            // change i < bufferSize to i < findNewLine()
         if(buffer[i] == '\n') {
-            this->start = nextFreeIndex();        // !!
+            this->start = i+1;        // !!
             count--;
             return return_string;
         }
-//        if(i  == bufferSize) {
-//            loop = true;
-//            break;
-//        }
-
-//        if(buffer[i] == 0)                // Ask if we must terminate as well if we read an empty space.
-//            return return_string;
 
         return_string += buffer[i];
         count--;
     }
-//    if(loop) {
-        for (int j = 0; j < start; j++) {
-            if (buffer[j] == '\n') {
-                this->start = nextFreeIndex();
-                count--;
-                return return_string;
-            }
-            return_string += buffer[j];
+
+    for (int j = 0; j < start; j++) {
+        if (buffer[j] == '\n') {
+            this->start = nextFreeIndex();
             count--;
-
+            return return_string;
         }
-//    }
+        return_string += buffer[j];
+        count--;
 
-    // Change the starting position to 1 element after the end-line
-//    std::cout << findNewline();
-
-//    this->start = findNewline() + 1;        // !!
-//    std::cout << start << std::endl;
-//    std::cout << this->findNewline() + 1 << std::endstartl;
-//    std::cout << this->start << std::endl;
-
+    }
     return "Error while reading line!\n";
 }
