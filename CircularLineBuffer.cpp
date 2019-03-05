d3ef3e0875dd92ba398b615714ce32d960b6b494
TLWNL <dylanrijnsburger@gmail.com>
2019-03-05 15:47:51 +0100

    Added a lot of 'this->'

--- CircularLineBuffer.cpp
+++ CircularLineBuffer.cpp
@@ -8,44 +8,44 @@
 #include <iostream>
 
 int CircularLineBuffer::freeSpace() {
-    return bufferSize - count;
+    return bufferSize - this->count;
 }
 
 bool CircularLineBuffer::isFull(){
-    return count == bufferSize;
+    return this->count == bufferSize;
 }
 
 bool CircularLineBuffer::isEmpty() {
-    return count == 0;
+    return this->count == 0;
 }
 
 int CircularLineBuffer::nextFreeIndex() {
-    if(isFull())
+    if(this.isFull())
         return -1;
 
-    return start + count;
+    return this->start + this->count;
 }
 
 int CircularLineBuffer::findNewline() {
-    for(int i = start; i < bufferSize; i++) {
-        if(buffer[i] == '\n')
+    for(int i = this->start; i < bufferSize; i++) {
+        if(this->buffer[i] == '\n')
             return i;
     }
-    for(int j = 0; j < start; j++) {
-        if(buffer[j] == '\n')
+    for(int j = 0; j < this->start; j++) {
+        if(this->buffer[j] == '\n')
             return j;
     }
     return -1;
 }
 
 bool CircularLineBuffer::hasLine() {
-    return findNewline() > 0;
+    return this.findNewline() > 0;
 }
 
 bool CircularLineBuffer::_writeChars(const char *chars, size_t nchars) {
     bool loop = false;
     int chars_written = 0;
-    int start_point = nextFreeIndex();
+    int start_point = this.nextFreeIndex();
 
     if(nchars > freeSpace()) {
         std::cout << "Not enough space in the buffer!\n";
@@ -57,8 +57,8 @@ bool CircularLineBuffer::_writeChars(const char *chars, size_t nchars) {
             loop = true;
             break;
         }
-        buffer[i] = chars[chars_written];
-        count++;
+        this->buffer[i] = chars[chars_written];
+        this->count++;
         chars_written++;
     }
     if(loop){
@@ -66,11 +66,11 @@ bool CircularLineBuffer::_writeChars(const char *chars, size_t nchars) {
         int chars_left = nchars - chars_written;
 
         while(chars_left != 0){
-            magnie = (start+count) - bufferSize;
-            buffer[magnie] = chars[chars_written];
+            magnie = (this->start+this->count) - bufferSize;
+            this->buffer[magnie] = chars[chars_written];
             chars_written++;
             chars_left--;
-            count++;
+            this->count++;
         }
 
     }
@@ -80,34 +80,35 @@ bool CircularLineBuffer::_writeChars(const char *chars, size_t nchars) {
 std::string CircularLineBuffer::_readLine() {
     std::string return_string;
 
-    if(findNewline() == -1)
+    if(this.findNewline() == -1)
         return return_string;               // Return empty string if there is no full line
 
 
-    if(findNewline() < start){
-        for(int e = start; e < bufferSize; e++){
-            return_string += buffer[e];
-            count --;
+    if(findNewline() < this->start){
+        for(int e = this->start; e < bufferSize; e++){            // Loops from start to end of buffer
+            return_string += buffer[e];                     // Append character to return string
+            this->count --;                                       // Decrease count
         }
+        // Append elements from start of the buffer until '\n' is read
         int d = 0;
-        while(buffer[d] != '\n'){
+        while(this->buffer[d] != '\n'){
             return_string += buffer[d];
-            count --;
+            this->count --;
             d++;
         }
-        if(buffer[d] == '\n')
-            count --;
+        if(this->buffer[d] == '\n')
+            this->count --;                                       // Makes sure count is correct
     }
 
-    for(int i = start; i < findNewline() +1; i++){            // change i < bufferSize to i < findNewLine()
-        if(buffer[i] == '\n') {
-            this->start = i+1;        // !!
-            count--;
+    for(int i = this->start; i <= findNewline(); i++){          // No loop in buffer, so append 'normally'
+        if(this->buffer[i] == '\n') {
+            this->start = i+1;
+            this->count--;
             return return_string;
         }
 
-        return_string += buffer[i];
-        count--;
+        return_string += this->buffer[i];
+        this->count--;
     }
 
     return return_string;
